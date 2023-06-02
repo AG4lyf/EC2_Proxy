767ce1a81f09c2c924fa58a8c388b28279f25d60
AG4lyf <surajbhari159@gmail.com>
2023-06-02 12:17:16 +0530

    rename perfect

--- ec2_proxy/__init__.py
+++ ec2_proxy/__init__.py
@@ -1,5 +1,7 @@
-"""Top-level package for EC2 Proxy."""
+"""Top-level package for Temp Proxy EC2."""
 
 __author__ = """Suraj Bhari"""
 __email__ = 'surajbhari159@gmail.com'
 __version__ = '0.1.0'
+
+from .ec2_proxy import TProxy
